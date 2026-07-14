c6fbcefb60800f3677d23b1c8ea45b06c434f082
Kamil Jan Mularski <kontakt@koder95.pl>
2026-07-14 22:48:58 +0200

    Remove unnecessary magic link implementations
    Removed packages from `main` and `test`:
    - `pl.koder95.sbp.backend.security.ml`
    - `pl.koder95.sbp.backend.security.ml.impl`
    Removed packages from `test`:
    - `pl.koder95.sbp.backend.service`
    - `pl.koder95.sbp.backend.factory`
    Removed classes and interfaces from `main`:
    - `pl.koder95.sbp.backend.factory.impl.MagicLinkFactoryImpl`
    - `pl.koder95.sbp.backend.factory.impl.MagicTokenFactoryImpl`
    - `pl.koder95.sbp.backend.factory.MagicLinkFactory`
    - `pl.koder95.sbp.backend.factory.MagicTokenFactory`
    - `pl.koder95.sbp.backend.service.impl.MagicLinkServiceImpl`
    - `pl.koder95.sbp.backend.service.MagicLinkService`

--- src/main/java/pl/koder95/sbp/backend/factory/MagicLinkFactory.java
+++
@@ -1,8 +0,0 @@
-package pl.koder95.sbp.backend.factory;
-
-import java.util.UUID;
-import pl.koder95.sbp.backend.security.ml.MagicLink;
-
-public interface MagicLinkFactory {
-    MagicLink createMagicLink(String emailAddress, UUID notificationUuid);
-}
