9c4c435de2a8827d8f880e32e619f977c0686306
Kamil Jan Mularski <kontakt@koder95.pl>
2026-08-18 22:03:21 +0200

    Prevent create LessonSpecification instance

--- src/main/java/pl/koder95/sbp/backend/specification/LessonSpecification.java
+++ src/main/java/pl/koder95/sbp/backend/specification/LessonSpecification.java
@@ -4,6 +4,9 @@ import pl.koder95.sbp.backend.dto.LessonSearchParamsDto;
 import pl.koder95.sbp.backend.model.Lesson;
 
 public final class LessonSpecification {
+    private LessonSpecification() {
+        throw new IllegalStateException("This constructor cannot be called");
+    }
 
     public interface Builder extends SpecificationBuilder<Lesson, LessonSearchParamsDto> {
     }
