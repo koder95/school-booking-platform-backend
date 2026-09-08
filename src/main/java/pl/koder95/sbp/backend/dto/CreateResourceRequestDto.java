071f62a27eee5e8a14c2188bbd1b8b00df11e5ec
Kamil Jan Mularski <kontakt@koder95.pl>
2026-09-09 00:45:31 +0200

    Improve OpenAPI schema info

--- src/main/java/pl/koder95/sbp/backend/dto/CreateResourceRequestDto.java
+++ src/main/java/pl/koder95/sbp/backend/dto/CreateResourceRequestDto.java
@@ -1,10 +1,32 @@
 package pl.koder95.sbp.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.URL;
 import pl.koder95.sbp.backend.model.Resource.Type;
 
+@Schema(
+        title = "Request to create a new resource",
+        description = "Data transfer object for creating a new resource."
+)
 public record CreateResourceRequestDto(
-        @URL String url,
-        Type type
+        @Schema(
+                title = "Resource URL",
+                description = "The URL of the resource to be created.",
+                example = "https://frontend.example.com/resources/avatar.png"
+        )
+        @NotNull @URL String url,
+        @Schema(
+                title = "Resource type",
+                description = """
+                The type of the resource to be created.
+                It is used to categorize the resource
+                and determine its significance within the system.
+                """,
+                examples = {
+                        "AVATAR"
+                }
+        )
+        @NotNull Type type
 ) {
 }
--- src/main/java/pl/koder95/sbp/backend/dto/ResourceDto.java
+++ src/main/java/pl/koder95/sbp/backend/dto/ResourceDto.java
@@ -1,11 +1,33 @@
 package pl.koder95.sbp.backend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.UUID;
 import pl.koder95.sbp.backend.model.Resource.Type;
 
+@Schema(
+        title = "Response with a resource",
+        description = "Data transfer object representing a resource."
+)
 public record ResourceDto(
+        @Schema(
+                title = "Resource UUID",
+                description = "The unique identifier of the resource.",
+                example = "123e4567-e89b-12d3-a456-426614174000"
+        )
         UUID uuid,
+        @Schema(
+                title = "Resource URL",
+                description = "The URL of the resource.",
+                example = "https://frontend.example.com/resources/avatar.png"
+        )
         String url,
+        @Schema(
+                title = "Resource type",
+                description = "The type of the resource.",
+                examples = {
+                        "AVATAR"
+                }
+        )
         Type type
 ) {
 }
